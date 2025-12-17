f3c600ee4c37ee121214c7d079e5d6de6a02f9db
DrewCyber <DruCyber@gmail.com>
2025-12-17 19:49:11 +0400

    Split apk assets build for every platform.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,6 +137,15 @@ android {
         }
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
